989b55e1de66d2892e4b092333e61bf388b2d6d1
N Chaitra1 <1099395@India.TCS.com>
2017-06-07 04:29:15 -0700

    add new script - docker build

---
+++ scripts/docker_build.bat
@@ -0,0 +1,9 @@
+cd %1/%2
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="tcp://192.168.99.100:2376"
+export DOCKER_CERT_PATH="C:\Users\1099395\.docker\machine\machines\default"
+export DOCKER_MACHINE_NAME="default"
+export COMPOSE_CONVERT_WINDOWS_PATHS="true"
+# Run this command to configure your shell:
+# eval $("C:\Program Files\Docker Toolbox\docker-machine.exe" env default)
+mvn docker:build
