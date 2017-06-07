d7e19b52025d2821fee3eab2c8cc5591fccfb284
1099395 <n.chaitra1@tcs.com>
2017-06-07 04:33:54 -0700

    Update docker_build.bat

--- scripts/docker_build.bat
+++ scripts/docker_build.bat
@@ -1,4 +1,4 @@
-cd %1/%2
+cd C:\\chaitra\\login-service
 export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="tcp://192.168.99.100:2376"
 export DOCKER_CERT_PATH="C:\Users\1099395\.docker\machine\machines\default"
