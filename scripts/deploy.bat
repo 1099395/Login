ba281e74e32716847ae07ce52b83faa5cf1e4088
1099395 <n.chaitra1@tcs.com>
2017-06-08 06:36:41 -0700

    Create deploy.bat

--- scripts/deploy.bat
+++ scripts/deploy.bat
@@ -1,9 +1,3 @@
-SET DOCKER_TLS_VERIFY=1
-SET DOCKER_HOST=tcp://192.168.99.103:2376
-SET DOCKER_CERT_PATH=C:\Users\1099395\.docker\machine\machines\default
-SET DOCKER_MACHINE_NAME=default
-SET COMPOSE_CONVERT_WINDOWS_PATHS=true
 cd %1
-minikube start
 kubectl create -f deployment.yml
 kubectl create -f loginimage.yml
