1ddfa50e45b72713c5b5f7f72525fd541b4a1757
1099395 <n.chaitra1@tcs.com>
2017-06-08 23:16:09 -0700

    Create push_image.bat

--- scripts/push_image.bat
+++ scripts/push_image.bat
@@ -1,7 +1,7 @@
-cd C:\chaitra\login-service
+cd C:\Drishya\login-service
 SET DOCKER_TLS_VERIFY=1
 SET DOCKER_HOST=tcp://192.168.99.100:2376
-SET DOCKER_CERT_PATH=C:\Users\1099395\.docker\machine\machines\default
+SET DOCKER_CERT_PATH=C:\Users\1114446\.docker\machine\machines\default
 SET DOCKER_MACHINE_NAME=default
 SET COMPOSE_CONVERT_WINDOWS_PATHS=true
 REM Run this command to configure your shell:
